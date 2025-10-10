63b8edb5fa0968fea9e911e63a581449b8cb7cac
a-ayushi <aayushipatidar07@gmail.com>
2025-10-10 18:54:20 +0530

    Exceptional handling code added

---
+++ src/main/java/com/sagargarrie/exception/BookingNotFoundException.java
@@ -0,0 +1,7 @@
+package com.sagargarrie.exception;
+
+
+public class BookingNotFoundException extends RuntimeException{
+    public BookingNotFoundException(String msg) { super(msg); }
+
+}
---
+++ src/main/java/com/sagargarrie/exception/UnauthorizedException.java
@@ -0,0 +1,6 @@
+package com.sagargarrie.exception;
+
+public class UnauthorizedException extends RuntimeException{
+    public UnauthorizedException(String msg) { super(msg); }
+
+}
